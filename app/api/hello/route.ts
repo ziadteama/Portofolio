c6054cf7f504283327b87cffde7295907ed0b751
Christian Martinez <hello@christianbmartinez.com>
2024-12-22 07:26:43 -0700

    Api

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
-export const GET = (req: NextRequest) => {
-    const request = req.body
-    return NextResponse.json({ hello: `world. You sent: ${request}!` })
+export const GET = async (req: NextRequest) => {
+    return NextResponse.json({ hello: "world" })
 }
