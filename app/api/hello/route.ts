754d613b0ef24d908c692d3efd378bb8406b95df
Christian Martinez <hello@christianbmartinez.com>
2024-12-22 07:33:00 -0700

    Next response was declared but never used

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export const GET = async (req: NextRequest) => {
+export const GET = async () => {
     return NextResponse.json({ hello: "world" })
 }
